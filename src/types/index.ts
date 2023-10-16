0bc648b1b6ca62a5740f4b13570cb77853501fe3
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-10-17 06:32:36 +0700

    feat: add necessary type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ApolloQueryResult } from "@apollo/client"
+
 import { formatContactList } from "@/lib"
 
 export type FormattedContactList = ReturnType<typeof formatContactList>
+
+export type AddPersonBody = {
+  firstName: string
+  lastName: string
+  phones: { number: string }[]
+}
+
+export interface QueryResult<TData> {
+  data?: TData
+  error?: any
+  loading: boolean
+}
+
+export interface QueryContextValue<TData> {
+  queryData: QueryResult<TData>
+  refetch: () => Promise<ApolloQueryResult<TData>>
+}
